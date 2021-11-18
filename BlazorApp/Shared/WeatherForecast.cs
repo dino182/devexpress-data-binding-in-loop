dc19110538031c786fff5d63950d3b02e9fd0d70
Dino Infantino <dino.infantino@displayplan.com>
2021-11-18 14:48:02 +0000

    Add demo code

--- BlazorApp/Shared/WeatherForecast.cs
+++ BlazorApp/Shared/WeatherForecast.cs
@@ -13,5 +13,7 @@ namespace BlazorApp.Shared
         public string Summary { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public bool Visible { get; set; }
     }
 }
